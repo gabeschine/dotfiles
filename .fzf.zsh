714f10e27b135d77186089e2994aa5cd7508bdb3
Gabe Schine <gabe@schine.net>
2023-05-08 16:47:18 -0700

    Fix fzf init script:

--- .fzf.zsh
+++ .fzf.zsh
@@ -7,7 +7,7 @@ fi
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
-[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/key-bindings.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
